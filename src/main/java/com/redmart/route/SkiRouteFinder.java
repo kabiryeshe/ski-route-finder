2cc67f30dc254f47ab92789a70143cff56e32ba7
Tushar <tusharg@thoughtworks.com>
2016-05-07 12:53:57 +0530

    first commit - gradle task to run jar

---
+++ src/main/java/com/redmart/route/SkiRouteFinder.java
@@ -0,0 +1,8 @@
+package com.redmart.route;
+
+public class SkiRouteFinder {
+
+    public static void main(String []args) {
+        System.out.println("Basic Setup for Ski route finder");
+    }
+}
